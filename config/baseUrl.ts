e529777863054a001d5302b49da777ee5700d5b9
hmzsumon <zsumonn@gmail.com>
2025-02-25 17:15:19 +0600

    first commit

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
 	console.log('Running in development mode');
 } else {
 	//Global-X-api-1901b8e6064a.herokuapp.com/
-	https: baseUrl = 'https://pyc-trade-api-329967f9dc4f.herokuapp.com';
+	https: baseUrl = 'https://pyc-trade-api-de5130f4c952.herokuapp.com';
 	// Code specific to production mode
 	console.log('Running in production mode');
 }
